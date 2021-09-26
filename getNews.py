f7a335a0ccd347851360fe6437dcdc2eca99ff2e
kei1115 <keifukuda1115@gmail.com>
2021-09-27 04:22:03 +0900

    Delete get_stock.py and getNews.py

--- getNews.py
+++
@@ -1,9 +0,0 @@
-from newsapi import NewsApiClient
-
-# Init
-newsapi = NewsApiClient(api_key='f08c6d20b4e547b2a3fd42657a63fdc6')
-
-headlines = newsapi.get_top_headlines(sources='techcrunch')
-
-# 取得したトップニュースの１件を表示
-print(headlines['articles'][0])
--- get_stock.py
+++
@@ -1,4 +0,0 @@
-from pandas_datareader import data
-
-df = data.DataReader("7203.T","yahoo")
-print(df.iloc['2021-09-11'])

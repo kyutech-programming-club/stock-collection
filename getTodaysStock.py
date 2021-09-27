253f27ecf985dc29c588b7f2693a49879d6a47d9
hibiki1228 <devkimadore1228@gmail.com>
2021-09-27 15:04:25 +0900

    print date

--- getTodaysStock.py
+++ getTodaysStock.py
@@ -9,19 +9,22 @@ end = dt.date.today()
 start = end - timedelta(days=365)
 
 df = web.DataReader('7203.T', "yahoo", start, end)
-
+# print(df)
 df2 = pd.DataFrame({'TOYOTA': df['Close']})
 # print(type(df2))
-
+# print(df2)
 s = df2.values.tolist()
+Date = df2.reset_index().values.tolist()
+print(Date)
 # print(s)
 # print(len(s))
+# print(df['Date'])
 
 v = []
 
 for i in range(len(s)):
     v.append(s[i][0])
-
+# print(v)
 
 df2.plot(title='TOYOTA', grid=True)
 plt.show()

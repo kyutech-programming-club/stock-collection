c446dd4a84ac7492129f30badafe24fbe503c8b6
kei1115 <keifukuda1115@gmail.com>
2021-10-04 17:59:32 +0900

    基準を割合化、返り値をリスト化、関数化

--- getTodaysStock.py
+++ getTodaysStock.py
@@ -5,25 +5,36 @@ import numpy as np
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 
-end = dt.date.today()
-start = end - timedelta(days=365)
+def searchDates():
+    end = dt.date.today()
+    start = end - timedelta(days=365)
 
-df = web.DataReader('7203.T', "yahoo", start, end)
-df2 = pd.DataFrame({'TOYOTA': df['Close']})
+    df = web.DataReader('7203.T', "yahoo", start, end)
+    df2 = pd.DataFrame({'TOYOTA': df['Close']})
 
-stocks = np.array(df2.values.tolist())
-dates = df2.reset_index().values.tolist()
-base = 810
+    stocks = np.array(df2.values.tolist())
+    dates = df2.reset_index().values.tolist()
 
-for num in range(240):
-    if abs(stocks[num] - stocks[num+4]) > base or abs(stocks[num] - stocks[num+3]) > base or abs(stocks[num] - stocks[num+2]) > base or abs(stocks[num] - stocks[num+1]) > base:
-        print(dates[num]) 
+    sum = 0
+    base = 0
+    print(df)
+    for num in range(230):
+        sum += stocks[num]
+        average = sum / 230
+        base = average * 0.08
 
-v = []
+    for num in range(230):
+        if abs(stocks[num] - stocks[num+4]) > base or abs(stocks[num] - stocks[num+3]) > base or abs(stocks[num] - stocks[num+2]) > base or abs(stocks[num] - stocks[num+1]) > base:
+            dateData = dates[num][0].strftime('%Y年%m月%d日') 
+            dateList = []
+            dateList.append(dateData)
+            print(dateData)
+        
+# v = []
 
-for i in range(len(stocks)):
-    v.append(stocks[i][0])
-# print(v)
+# for i in range(len(stocks)):
+#     v.append(stocks[i][0])
+# # print(v)
 
-df2.plot(title='TOYOTA', grid=True)
-plt.show()
+# df2.plot(title='TOYOTA', grid=True)
+# plt.show()
